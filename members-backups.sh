ad11d7dadfb8139ffbfe51f33c56268b528b86da
Philip Champon <pchampon@gmail.com>
2015-08-10 00:25:04 -0400

    Update members-backups.sh
    so that's why the script keeps disappearing ... derp! :/

--- members-backups.sh
+++ members-backups.sh
@@ -8,4 +8,4 @@ mysqldump -u $user $db $args | bzip2 > $backup
 rm -f $latest
 ln -sf $backup $latest
 scp $latest bushwickfoodcoop@june.mayfirst.org:backups/${latest##*/}.sql.bz2
-find /opt/backups/ -name \*members\* -mtime +10 -delete
+find /opt/backups/ -name \*members\*bz2 -mtime +10 -delete
