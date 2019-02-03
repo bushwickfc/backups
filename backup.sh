d5e1b9432acb95ce00b829974f06da0c40884b27
Darren Klein <darren.a.klein@gmail.com>
2019-02-03 10:14:24 -0500

    Updated the POS backup script for our current server setup.

--- backup.sh
+++ backup.sh
@@ -4,15 +4,11 @@ db=${1:?Missing db}
 user=${2:?Missing user}
 pass=${3:?Missing pass}
 host=${4:-viewsic.mayfirst.org}
-top_level=/home/members/bfc/sites/bushwickfoodcoop.org/users/bushwickfoodcoop/backups
+top_level=/home/members/bfc/sites/db.bushwickfoodcoop.org/users/tmp/db.bushwickfoodcoop.org/backups
 backup=$top_level/$db.bz2
-remote_backup=/opt/backups/"$(date +%s)-$(basename $backup)"
 if [ "$db" = "pos" ]; then
   tables=$(mysqlshow -u $user -p"$pass" -h $host $db \* | grep [A-Z][A-Z] | sed 's/[^_A-Z]//g')
   args="--tables $tables"
 fi
 
 mysqldump -u $user -p"$pass" -h $host $db $args | bzip2 > $backup
-
-scp $backup root@db.ov.bushwickfoodcoop.org:$remote_backup
-ssh root@db.ov.bushwickfoodcoop.org /opt/backups/cp-backup.sh $remote_backup pos
--- pg_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-db="owners_db"
-backup="/Users/darrenklein/Desktop/$db.bz2"
-
-pg_dump $db | bzip2 > $backup
