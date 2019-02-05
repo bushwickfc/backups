9ec58c5898ad8a867aed67959b5008edd3e5b7ce
Darren Klein <darren.a.klein@gmail.com>
2019-02-05 08:40:45 -0500

    Update script for our current server, update README.

--- README.md
+++ README.md
@@ -4,9 +4,16 @@ Backup scripts for various databases.
 
 ## backup.sh
 
-**backup.sh** goes on Mayfirst
+**backup.sh** goes on Mayfirst.
+
+It is currently deployed at `/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh` and writes backups to `/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backups/$db.bz2`.
+
+The backup is set to run as a cronjob by the user `dbbwick` daily at midnight with the following crontab command:
+
+```
+0 0 * * * /home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh $db $user $password
+```
 
 ## cp-backup.sh
-
 
 **cp-backup.sh** goes on Open Cirtuals
--- backup.sh
+++ backup.sh
@@ -3,12 +3,12 @@ set -ex
 db=${1:?Missing db}
 user=${2:?Missing user}
 pass=${3:?Missing pass}
-host=${4:-viewsic.mayfirst.org}
-top_level=/home/members/bfc/sites/db.bushwickfoodcoop.org/users/tmp/db.bushwickfoodcoop.org/backups
+host=${4:-pos.bushwickfoodcoop.org}
+top_level=/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backups
 backup=$top_level/$db.bz2
 if [ "$db" = "pos" ]; then
   tables=$(mysqlshow -u $user -p"$pass" -h $host $db \* | grep [A-Z][A-Z] | sed 's/[^_A-Z]//g')
   args="--tables $tables"
 fi
 
-mysqldump -u $user -p "$pass" -h $host $db $args | bzip2 > $backup
+mysqldump -u $user -p"$pass" -h $host $db $args | bzip2 > $backup
