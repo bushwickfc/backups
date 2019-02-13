8041b630639b01b57aba9e292672f0837b5e131f
Darren Klein <darren.a.klein@gmail.com>
2019-02-13 02:31:21 -0500

    Able to extract connection string from the Heroku config JSON.

--- pg-test.sh
+++ pg-test.sh
@@ -6,5 +6,12 @@ get_heroku_config() {
 }
 
 heroku_config=$( get_heroku_config ${oauth_token} )
+# We receive the Heroku config as a JSON string, and will need to find the connection string inside.
+# The regex requires using a lookbehind, which does not work with grep unless the -P, --perl-regexp option is available.
+# I'm developing on a Mac, which doesn't have that option, and the Mayfirst server warns
+# "This is highly experimental and grep -P may warn of unimplemented features."
+# However, there is an alternative, which is to use the perl command -
+# https://stackoverflow.com/questions/16658333/grep-p-no-longer-works-how-can-i-rewrite-my-searches
+pg_conn_string=$( echo $heroku_config | perl -nle'print $& while m{(?<="HEROKU_POSTGRESQL_COBALT_URL":)"\S+"}g' | tail -1 )
 
-echo $heroku_config
+echo $pg_conn_string
