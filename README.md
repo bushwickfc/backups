243dcae1a9a75639fc10de14a93256a37ce91e76
Darren Klein <darren.a.klein@gmail.com>
2019-02-13 20:35:19 -0500

    Sketch out conditional logic for handling either pos or owners_db updates - UNTESTED

--- README.md
+++ README.md
@@ -4,49 +4,40 @@ Backup scripts for various databases.
 
 ## backup.sh
 
-**backup.sh** goes on Mayfirst.
+**backup.sh** is hosted on Mayfirst, and it is used to back up the BFC's `pos` and `owners_db` databases, which are MySQL and PostgreSQL, respectively.
 
 It is currently deployed at `/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh` and writes backups to `/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backups/$db.bz2`.
 
 The backup is set to run as a cronjob by the user `dbbwick` daily at midnight with the following crontab command:
 
 ```
-0 0 * * * /home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh $db $user $password
+0 0 * * * /home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh pos $user $password
+0 0 * * * /home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backup.sh owners_db $oauth_token
 ```
+
+### pos
+
+The **pos** MySQL database is currently running on an AWS instance. To run the cronjob above, you'll need to provide values for the `$user` and `$password` arguments.
 
 ### owners_db - WORK IN PROGRESS
 
-The **owners_db** Postgres database is currently running on Heroku. Heroku periodically rotates DB credentials, and so the first part of the backup script retrieves those credentials from Heroku.
-
-In order to access the credentials via Heroku's platform API, the request will need to include an OAuth token associated with an account that has access to the database.
+The **owners_db** Postgres database is currently running on Heroku. Heroku periodically rotates DB credentials, and so the first part of the backup script retrieves those credentials from Heroku. In order to access the credentials via Heroku's platform API, the request will need to include an OAuth token associated with an account that has access to the database; thus, you'll need to provide a value for the `$oauth_token` argument.
 
 To generate an OAuth token via the Heroku CLI, see https://devcenter.heroku.com/articles/platform-api-quickstart
 
-The command
+To list your tokens via the Heroku CLI, run the command:
 
 ```bash
 heroku authorizations
 ```
 
-will list all current tokens.
+To see info (including the token itself) for a specific token, use the command:
 
 ```bash
 heroku authorizations:info $TOKEN_ID
 ```
 
 will show the info for that token.
-
-At the moment, experimenting with running this with bash and Python. So the command is either
-
-```bash
-./pg-test.sh $oauth_token
-```
-
-or
-
-```bash
-python3 pg-test.py $oauth_token
-```
 
 ## cp-backup.sh
 
--- pg-test.sh
+++ pg-test.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
-oauth_token=${1:?Missing OAuth token}
-
+set -ex
+db=${1:?Missing db}
 top_level=/home/members/bfc/sites/db.bushwickfoodcoop.org/users/dbbwickfood/backups
-backup=$top_level/owners_db.bz2
+backup=$top_level/$db.bz2
 
-get_heroku_config() {
-	echo $( curl -n https://api.heroku.com/apps/bfc-owners/config-vars -H "Authorization: Bearer $1" -H "Accept: application/vnd.heroku+json; version=3" )
-}
+if [ "$db" == "pos" ]
+then
+	# Back up the pos MySQL database
+	user=${2:?Missing user}
+	pass=${3:?Missing pass}
+	host=${4:-pos.bushwickfoodcoop.org}
 
-heroku_config=$( get_heroku_config ${oauth_token} )
-# We receive the Heroku config as a JSON string, and will need to find the connection string inside.
-# The regex requires using a lookbehind, which does not work with grep unless the -P, --perl-regexp option is available.
-# I'm developing on a Mac, which doesn't have that option, and the Mayfirst server warns
-# "This is highly experimental and grep -P may warn of unimplemented features."
-# However, there is an alternative, which is to use the perl command -
-# https://stackoverflow.com/questions/16658333/grep-p-no-longer-works-how-can-i-rewrite-my-searches
-# The regex will grab the value of the connection string, stripping off the double quotes.
-pg_conn_string=$( echo $heroku_config | perl -nle'print $& while m{(?<="HEROKU_POSTGRESQL_COBALT_URL":")([^"]*)}g' | tail -1 )
+	if [ "$db" = "pos" ]; then
+	  tables=$(mysqlshow -u $user -p"$pass" -h $host $db \* | grep [A-Z][A-Z] | sed 's/[^_A-Z]//g')
+	  args="--tables $tables"
+	fi
 
-pg_dump $pg_conn_string | bzip2 > $backup
+	mysqldump -u $user -p"$pass" -h $host $db $args | bzip2 > $backup
+else
+	# Back up the owners_db Postgres database
+	oauth_token=${2:?Missing OAuth token}
+
+	get_heroku_config() {
+		echo $( curl -n https://api.heroku.com/apps/bfc-owners/config-vars -H "Authorization: Bearer $1" -H "Accept: application/vnd.heroku+json; version=3" )
+	}
+
+	heroku_config=$( get_heroku_config ${oauth_token} )
+	# We receive the Heroku config as a JSON string, and will need to find the connection string inside.
+	# The regex requires using a lookbehind, which does not work with grep unless the -P, --perl-regexp option is available.
+	# I'm developing on a Mac, which doesn't have that option, and the Mayfirst server warns
+	# "This is highly experimental and grep -P may warn of unimplemented features."
+	# However, there is an alternative, which is to use the perl command -
+	# https://stackoverflow.com/questions/16658333/grep-p-no-longer-works-how-can-i-rewrite-my-searches
+	# The regex will grab the value of the connection string, stripping off the double quotes.
+	pg_conn_string=$( echo $heroku_config | perl -nle'print $& while m{(?<="HEROKU_POSTGRESQL_COBALT_URL":")([^"]*)}g' | tail -1 )
+
+	pg_dump $pg_conn_string | bzip2 > $backup
+fi
